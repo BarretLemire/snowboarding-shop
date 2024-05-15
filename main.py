a8cb165cf5d10fb18bdf81851fd750b025e08513
Barret Lemire <blemire4503@xmail.dixietech.edu>
2024-05-14 20:05:50 -0600

    fix endpoints:

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
 import json
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from enum import Enum
-from models import Brand, Snowboard
+from typing import List
+from models import Brand, Snowboard, NewSnowboard
 
-
-with open("snowboards.json", "r") as f:
-    snowboard_list = json.load(f)
 
 app = FastAPI()
 
-snowboard_list = list[Snowboard]
+with open("snowboards.json", "r") as f:
+    snowboard_data = json.load(f)
+    snowboard_list = [Snowboard(**snowboard) for snowboard in snowboard_data]
 
-@app.get("/snowboards")
+
+@app.get("/snowboards", response_model=List[Snowboard])
 async def get_snowboards() -> list[Snowboard]:
-    return snowboard_list.values()
+    return snowboard_list
 
 
-@app.post("/snowboards/new")
-async def new_snowboard(id: int, length: int, color: str, has_bindings: bool, Brand: Enum) -> list[Snowboard]:
-    return snowboard_list.append(id)
+@app.post("/snowboards/new", response_model=Snowboard)
+async def new_snowboard(snowboard: NewSnowboard) -> Snowboard:
+    new_snowboard = Snowboard(**snowboard.dict())
+    snowboard_list.append(new_snowboard)
+    return new_snowboard
 
 
 @app.put("/snowboards/{id}")
-async def update_snowboard(id: int, length: int, color: str, has_bindings: bool, brand: Enum) -> list[Snowboard]:
-    updated_snowboard = snowboard_list[id]
-    return id
+async def update_snowboard(id: int, length: int, color: str, has_bindings: bool, brand: Brand) -> Snowboard:
+    for snowboard in snowboard_list:
+        if snowboard.id == id:
+            snowboard.length = length
+            snowboard.color = color
+            snowboard.has_bindings = has_bindings
+            snowboard.brand = brand
+            return snowboard
+    raise HTTPException(status_code=404, detail="Snowboard not found")
+
 
 
 @app.delete("/snowboards/{id}")
-async def delete_snowboard(id: int) -> list[Snowboard]:
-    snowboard_list.pop(id)
+async def delete_snowboard(id: int) -> Snowboard:
+    for index, snowboard in enumerate(snowboard_list):
+        if snowboard.id == id:
+            deleted_snowboard = snowboard_list.pop(index)
+            return deleted_snowboard
+    raise HTTPException(status_code=404, detail="Snowboard not found")
--- models.py
+++ models.py
@@ -1,12 +1,21 @@
 from pydantic import BaseModel
 from enum import Enum
 
-class Brand(Enum):
-    brand: Enum
+class Brand(str, Enum):
+    Burton = "Burton"
+    Nitro = "Nitro"
+    Saloman = "Saloman"
 
 class Snowboard(BaseModel):
     id: int
     length: int
     color: str
     has_bindings: bool
-    brand: Enum
+    brand: Brand
+
+class NewSnowboard(BaseModel):
+    id: int
+    length: int
+    color: str
+    has_bindings: bool
+    brand: Brand
